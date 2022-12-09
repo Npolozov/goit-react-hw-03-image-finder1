f9ee2d3d3d2a5f0efe3b4ec955297e554786854e
Nikita Polozov <npolozov0@gmail.com>
2022-12-09 14:56:30 +0200

    1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,37 +2,38 @@ import { Searchbar } from 'components/Searchbar/Searchbar';
 import { Component } from 'react';
 import { imageByName } from 'api';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class App extends Component {
   state = {
     image: '',
     error: null,
     photo: [],
+    isLoading: false,
   };
 
   async componentDidMount() {}
 
-  handlelFormSubmit = async image => {
-    this.setState({ image });
-    const photo = await imageByName(image);
-    console.log(photo);
-    this.setState({ photo });
-  };
-
-  // selectBreed = async breedId => {
-  //   try {
-  //     this.setState({ isLoading: true, error: null });
-  //     const dog = await fetchDogByBreed(breedId);
-  //     this.setState({ dog });
-  //   } catch {
-  //     this.setState({
-  //       error:
-  //         'У нас не получилось взять данные о собачке, попробуйте еще разочек 😇',
-  //     });
-  //   } finally {
-  //     this.setState({ isLoading: false });
-  //   }
+  // handlelFormSubmit = async image => {
+  //   this.setState({ image });
+  //   const photo = await imageByName(image);
+  //   this.setState({ photo });
   // };
+
+  handlelFormSubmit = async image => {
+    try {
+      this.setState({ image, isLoading: true, error: null });
+      const photo = await imageByName(image);
+      this.setState({ photo });
+    } catch (error) {
+      toast.error(
+        'У нас не получилось взять данные о собачке, попробуйте еще разочек 😇'
+      );
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
 
   render() {
     const { error, image, photo } = this.state;
@@ -42,6 +43,7 @@ export class App extends Component {
         <Searchbar onSubmit={this.handlelFormSubmit} />
         {error && <p>{error}</p>}
         <ImageGallery hits={photo} />
+        <ToastContainer autoClose={2000} position="top-right" />
       </>
     );
   }
